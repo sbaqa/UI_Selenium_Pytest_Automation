2c27a9150063702add862c987552730f33e16741
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-09-18 13:46:13 +0300

    Update login_page_asserts.py

--- asserts/login_page_asserts.py
+++ asserts/login_page_asserts.py
@@ -1,6 +1,11 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from utils import utils as utils
-from helpers.generic_helpers import GenericHelpers
+from helpers.generic_helper import GenericHelpers
+from locators.login_signup_page_locators import LoginSignupLocators
+
+l_s_locators = LoginSignupLocators()
 
 class LoginAsserts:
 
@@ -29,3 +34,21 @@ class LoginAsserts:
     def assert_signup_full_form_visible(self):
         form_title = self.driver.find_element(By.XPATH, self.full_signup_form_xpath).is_displayed()
         assert form_title is True, f"ERROR! Actual boolean => {form_title}"
+
+    def incorrect_email_password_alert_shown(self, expected_alert_message: str):
+        found_alert_message = self.driver.find_element(
+            By.XPATH, value=f"{l_s_locators.incorrect_email_password_xpath % expected_alert_message}").text
+        assert expected_alert_message == found_alert_message, f"Actual found message => {found_alert_message}, " \
+                                                              f"expected alert message => {expected_alert_message}"
+
+    def successfully_created_account(self, expected_title: str):
+        actual_title = self.driver.find_element(By.XPATH, value=f"{l_s_locators.account_created_header_xpath}").text
+        assert expected_title == actual_title, f"Expected title => {expected_title}, but found => {actual_title}"
+        self.driver.find_element(By.XPATH, value=f"{l_s_locators.registration_continue_button_xpath}").click()
+
+    def user_name_is_visible(self, user_name):
+        user_name_button = WebDriverWait(self.driver, 5).until(
+            EC.visibility_of_element_located((By.XPATH, f"{l_s_locators.user_name_navbar_xpath % user_name}")))
+        assert user_name in user_name_button.text, f"Button was not visible and clickable, " \
+                                                   f"so boolean => {user_name_button}, name => {user_name_button.text}"
+        return user_name_button.text
