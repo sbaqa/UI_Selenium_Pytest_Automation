fbf0d675e9f897f009ab031b60efcc7b216fc0e8
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-09-18 11:09:01 +0300

    Create utils.py

---
+++ utils/utils.py
@@ -0,0 +1,18 @@
+# Constants
+import inspect
+
+URL = "https://automationexercise.com"
+INCORRECT_EMAIL = "test_user@test.com"
+INCORRECT_PASSWORD = "admin123"
+CORRECT_EMAIL = "user_test@test.com"
+CORRECT_PASSWORD = "Formulaq1w2e3!"
+login_form_header = "Login to your account"
+NAME = "test"
+DATE = "5"
+MONTH = "5"
+YEAR = "1990"
+ACCOUNT_CREATED = "ACCOUNT CREATED!"
+TEST_USER_NAME = "testuser"
+
+def whoami():
+    return inspect.stack()[1][3]
