7369bed45a392bd95dc9fb0b4d75e22279663b2f
Sviatoslav Bordovski <sbordovski@gmail.com>
2024-09-03 20:29:35 +0300

    Create home_page_locators.py

---
+++ locators/home_page_locators.py
@@ -0,0 +1,9 @@
+
+
+class HomePageLocators:
+    welcome_link_id = "welcome"
+    logout_link_linkText = "Logout"
+    visible_dashboard_id = "panel_draggable_1_0"
+    dashboard_link_id = "menu_dashboard_index"
+    cookies_title_header_css = "h1.fc-dialog-headline"
+    accept_cookies_button_xpath = "//button[@aria-label='Consent']"
