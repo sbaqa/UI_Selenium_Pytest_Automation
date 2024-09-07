48aefc985ad93cd9a57e3abc51190ecb87da8616
Sviatoslav Bordovski <sbordovski@gmail.com>
2024-09-07 20:52:36 +0300

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -13,6 +13,11 @@ MONTH = "5"
 YEAR = "1990"
 ACCOUNT_CREATED = "ACCOUNT CREATED!"
 TEST_USER_NAME = "testuser"
+COOKIES_HEADER = "This site asks for consent to use your data"
+DELETED_ACCOUNT_TITLE = "Account Deleted!"
+HOME_PAGE_TITLE = "Automation Exercise"
+NAVBAR_ACTIVE_LINK = "orange"
+INCORRECT_EMAIL_ALERT = "Your email or password is incorrect!"
 
-# def whoami():
-#     return inspect.stack()[1][3]
+def whoami():
+    return inspect.stack()[1][3]
