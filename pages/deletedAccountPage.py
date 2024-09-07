8ab88e54b5790fe7dbc1f588d4d12362429c2654
Sviatoslav Bordovski <sbordovski@gmail.com>
2024-09-07 20:51:18 +0300

    Update deletedAccountPage.py

--- pages/deletedAccountPage.py
+++ pages/deletedAccountPage.py
@@ -6,7 +6,13 @@ d_a_locators = DeletedAccountLocators()
 
 class DeletedAccount(DefaultSeleniumDriver):
 
+    def click_continue_button(self):
+        from pages.homePage import HomePage
+        self.driver.find_element(By.XPATH, value=f"{d_a_locators.continue_button_xpath}").click()
+
+        return HomePage(self.driver)
+
     def assert_account_deleted(self, expected_title: str):
-        actual_title = self.driver.find_element(By.XPATH, value=f"{d_a_locators.deleted_account_title_css}").text
-        assert expected_title == actual_title, (f"Found title '{actual_title}' does not "
-                                                f"equal expected '{expected_title}' title")
+        actual_title = self.driver.find_element(By.CSS_SELECTOR, value=f"{d_a_locators.deleted_account_title_css}").text
+        assert actual_title == expected_title.upper(), (f"Found title '{actual_title}' does not "
+                                                        f"equal expected '{expected_title}' title")
